584780459acf4dd38c78b2692b5cc1e6f1b854bc
TommyWaalkes <Tommy@grandcircus.co>
2017-07-14 11:35:52 -0400

    Intial Commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ShapeFactory s = new ShapeFactory();
+            IShape squ = s.GetShape("square");
+            squ.Draw();
+            IShape cir = s.GetShape("circle");
+            cir.Draw();
+            IShape tri = s.GetShape("triangle");
+            tri.Draw();
+        }
+    }
+}
---
+++ ShapeFactory.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPattern
+{
+        public class ShapeFactory
+        {
+            //use getShape method to get object of type shape
+            //build a square circle 
+            public IShape GetShape(string ShapeType)
+            {
+
+            if (ShapeType == "square")
+            {
+                return new Square();
+            }
+            else if (ShapeType == "circle")
+            {
+                return new Circle();
+            }
+            else if (ShapeType == "triangle")
+            {
+                return new Triangle();
+            }
+
+            else
+            {
+                return null;
+            }
+
+            }
+        }
+    }
